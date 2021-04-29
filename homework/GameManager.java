1e8d0944fb86cb5c1f615f13eb4a62241b5d1e96
yakupselami4 <82455020+yakupselami4@users.noreply.github.com>
2021-04-29 14:02:33 +0300

    Update GameManager.java

--- homework/GameManager.java
+++ homework/GameManager.java
@@ -2,7 +2,7 @@ package homework;
 
 public class GameManager {
 	public void PlayedGames(Game game) {
-		System.out.println("Oynandý:" +game.id);
+		System.out.println("Oynandi:" +game.id);
 		
 		
 	}
